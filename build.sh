77375eef8bd8c13e0e6caa7f00f83f9be58832b6
Matt McCormick <matt.mccormick@kitware.com>
2018-05-07 17:37:38 -0400

    ENH: tag Docker image with latest Git revision

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build -f Dockerfile.local -t thewtex/krs-course:local $DIR
-docker build -f Dockerfile.jupyterhub -t thewtex/krs-course:latest $DIR
+docker build -f Dockerfile.jupyterhub -t thewtex/krs-course:$(git rev-parse HEAD | cut -c1-7) $DIR
