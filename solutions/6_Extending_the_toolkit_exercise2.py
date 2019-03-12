655d73356a9c92fbcd3e4842c5b7ccca6386c557
Francois Budin <francois.budin@kitware.com>
2019-03-12 17:10:11 -0400

    Split 6_Extending_the_Toolkit in two parts
    Part 1: Introduction and presentation of existing modules
    Part 2: How to create a new module and CI integration

---
+++ solutions/6_Extending_the_toolkit_exercise2.py
@@ -0,0 +1,8 @@
+#* Open a Notebook terminal (New->Terminal)
+#* Explore module code
+#        cd ITKModuleTemplate
+#        ls
+#        cd include/
+#        ls
+#* Look at `itkMyFilter.hxx`
+#* Modify `itkMyFilter.hxx`
