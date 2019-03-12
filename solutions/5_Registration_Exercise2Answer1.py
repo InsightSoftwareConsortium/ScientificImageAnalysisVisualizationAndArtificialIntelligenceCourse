85d2859b88be1dfe646bb3ebdb38cc498fa1d35a
Dženan Zukić <dzenan.zukic@kitware.com>
2019-03-11 20:26:46 -0400

    ENH: using %load for exercise answers and polishing the rise slides

---
+++ solutions/5_Registration_Exercise2Answer1.py
@@ -0,0 +1,5 @@
+registration = itk.ImageRegistrationMethodv4.New(FixedImage=fixedImage,
+        MovingImage=movingImage,
+        Metric=metric,
+        Optimizer=optimizer,
+        InitialTransform=initialTransform)
