2cfbfc40d197aa013086411cf8b2611405bbd24a
Matt McCormick <matt.mccormick@kitware.com>
2019-03-13 02:12:37 -0400

    ENH: Add Extending the Toolkit Part 1 exercise

---
+++ solutions/6_Extending_the_toolkit_exercise.py
@@ -0,0 +1,8 @@
+textures = itk.coocurrence_texture_features_image_filter(image,
+                                                         mask_image=mask,
+                                                         number_of_bins_per_axis=10,
+                                                         histogram_minimum=0,
+                                                         histogram_maximum=4200,
+                                                         neighborhood_radius=6)
+texture_array = itk.array_view_from_image(textures)
+view(texture_array[:,:,:,1].reshape(texture_array.shape[:-1]).copy(), mode='z', cmap='jet')
