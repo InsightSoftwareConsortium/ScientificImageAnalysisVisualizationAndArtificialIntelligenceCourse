0e7ccc7af24079ee79ae875390eb18d46e18fcfc
Francois Budin <francois.budin@kitware.com>
2019-03-14 16:57:14 -0400

    Correct minor bugs and improve one example

--- solutions/3_Segmentation_Exercise2.py
+++ solutions/3_Segmentation_Exercise2.py
@@ -1,8 +1,8 @@
 input_image = itk.imread('data/BrainProtonDensitySlice.png', itk.ctype('unsigned char'))
 isolated_connected = itk.IsolatedConnectedImageFilter.New(input_image)
-isolated_connected.SetSeed1([85, 115])
-isolated_connected.SetSeed2([89, 154])
-isolated_connected.SetUpperValueLimit( 255 )
+isolated_connected.SetSeed1([98, 112])
+isolated_connected.SetSeed2([98, 136])
+isolated_connected.SetUpperValueLimit( 245 )
 isolated_connected.FindUpperThresholdOff();
 isolated_connected.Update()
 
